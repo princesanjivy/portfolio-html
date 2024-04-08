0d67458be59cf2511886e9e9afbc2d81828b2840
princesanjivy <sanjivy.android@gmail.com>
2024-04-08 23:11:12 +0530

    initial commit

---
+++ assets/js/varDates.js
@@ -0,0 +1,8 @@
+// Prince Sanjivy
+
+let d = new Date();
+
+document.getElementById("coding-years").innerHTML = d.getFullYear() - 2021;
+document.getElementById("my-age").innerHTML = d.getFullYear() - 2001;
+document.getElementById("flutter-exp").innerHTML = d.getFullYear() - 2020;
+document.getElementById("python-exp").innerHTML = d.getFullYear() - 2018;
